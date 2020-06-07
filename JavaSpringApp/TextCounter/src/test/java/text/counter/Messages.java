587d57bb36622a714d9e31e93b76a91fb63a594e
Josip Bošnjak <jbosnjak@unipu.hr>
2020-06-07 13:32:30 +0200

    added nessages class
    added messages class which have function to print some value given by parameter.

---
+++ JavaSpringApp/TextCounter/src/test/java/text/counter/Messages.java
@@ -0,0 +1,19 @@
+package text.counter;
+
+
+/**
+ * 
+ * @author Korisnik
+ * @category Concept
+ * @since 7.6.2020. 13:31 
+ * @version 1.0
+ *
+ */
+
+public class Messages {
+
+static void printMessage(String value) {
+	System.out.println(value);
+}
+
+}
