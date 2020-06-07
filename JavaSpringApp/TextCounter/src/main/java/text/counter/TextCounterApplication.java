74bf8fc0da79bfcb825f1d837cbce5fc55d7669f
Josip Bošnjak <jbosnjak@unipu.hr>
2020-06-07 10:01:59 +0200

    initial app

---
+++ JavaSpringApp/TextCounter/src/main/java/text/counter/TextCounterApplication.java
@@ -0,0 +1,13 @@
+package text.counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextCounterApplication.class, args);
+	}
+
+}
---
+++ JavaSpringApp/TextCounter/src/test/java/text/counter/TextCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package text.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
