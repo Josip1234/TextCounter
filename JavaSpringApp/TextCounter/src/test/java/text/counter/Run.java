e4566965f5d8838115c0efb9e80a2416499caf6c
Josip Bošnjak <jbosnjak@unipu.hr>
2020-06-07 13:23:23 +0200

    added run class
    Added run class which will be used to implement word interface and also will be used as static void function to run main test appllication.

---
+++ JavaSpringApp/TextCounter/src/test/java/text/counter/Run.java
@@ -0,0 +1,21 @@
+package text.counter;
+
+import java.util.List;
+
+/***
+ * 
+ * @author Korisnik
+ * @category Concept
+ * @since 7.6.2020. 13:22
+ * @version 1.0
+ *
+ */
+public class Run implements Words {
+
+	@Override
+	public List<Word> addWord(String word) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
